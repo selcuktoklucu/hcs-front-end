02bbfe5874d38ebce85fe4a638d6d0569f3305f7
selcuktoklucu <selcuktoklucu@gmail.com>
2019-08-14 09:54:14 -0400

    Initial Commit Front End

---
+++ src/auth/components/SignIn.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+import { signIn } from '../api'
+import messages from '../messages'
+
+class SignIn extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = event => this.setState({
+    [event.target.name]: event.target.value
+  })
+
+  onSignIn = event => {
+    event.preventDefault()
+
+    const { alert, history, setUser } = this.props
+
+    signIn(this.state)
+      .then(res => setUser(res.data.user))
+      .then(() => alert(messages.signInSuccess, 'success'))
+      .then(() => history.push('/'))
+      .catch(error => {
+        console.error(error)
+        this.setState({ username: '', password: '' })
+        alert(messages.signInFailure, 'danger')
+      })
+  }
+
+  render () {
+    const { username, password } = this.state
+
+    return (
+      <form className='auth-form' onSubmit={this.onSignIn}>
+        <h3>Sign In</h3>
+        <label htmlFor="username">Username</label>
+        <input
+          required
+          type="username"
+          name="username"
+          value={username}
+          placeholder="Username"
+          onChange={this.handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          required
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={this.handleChange}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    )
+  }
+}
+
+export default withRouter(SignIn)
---
+++ src/header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Header.scss'
+
+const authenticatedOptions = (
+  <React.Fragment>
+    <Link to="/">Home</Link>
+    <Link to="/change-password">Change Password</Link>
+    <Link to="/sign-out">Sign Out</Link>
+  </React.Fragment>
+)
+
+const unauthenticatedOptions = (
+  <React.Fragment>
+    <Link to="/sign-up">Sign Up</Link>
+    <Link to="/sign-in">Sign In</Link>
+  </React.Fragment>
+)
+
+const alwaysOptions = (
+  <React.Fragment>
+  </React.Fragment>
+)
+
+const Header = ({ user }) => (
+  <header className="main-header">
+    <h1>My Tasks!</h1>
+    <nav>
+      { user && <span>Welcome, {user.username}</span>}
+      { user ? authenticatedOptions : unauthenticatedOptions }
+      { alwaysOptions }
+    </nav>
+  </header>
+)
+
+export default Header
